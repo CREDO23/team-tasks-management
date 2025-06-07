24ec6526c026795585fac1bf3af2998c752cb26a
CREDO23 <thierrybakera12@gmail.com>
2025-06-07 13:12:13 +0200

    implement a basic authorization logic

---
+++ src/common/guards/api-key/api-key.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthorizationTokenGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    const authToken = request.headers['authorization'];
+
+    if (!authToken) {
+      return false;
+    }
+
+    return true;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/exceptions/http.exeption';
+import { AuthorizationTokenGuard } from './common/guards/api-key/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,6 +16,7 @@ async function bootstrap() {
       },
     }),
   );
+  app.useGlobalGuards(new AuthorizationTokenGuard());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
